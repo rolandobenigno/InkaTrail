d71e871e2dad92df4d4e0d5c9150dcc2aee6a1bb
Rolando Benigno <rbenigno@gmail.com>
2019-06-21 00:58:57 -0500

    Capa REST API

--- Servicios/inkatrail/src/main/java/com/proyecto/inkatrail/negocio/Negocio.java
+++ Servicios/inkatrail/src/main/java/com/proyecto/inkatrail/negocio/Negocio.java
@@ -3,7 +3,11 @@ package com.proyecto.inkatrail.negocio;
 import com.proyecto.inkatrail.entidades.Reserva;
 import com.proyecto.inkatrail.repositorio.ReservaRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -26,15 +30,16 @@ public class Negocio {
 
     public Reserva actualizarReserva(Reserva reserva) throws Exception{
         Reserva p = reservaRepositorio.findById(reserva.getCodigo()).orElseThrow
-                (() -> new Exception("No se puede actualizar el producto"));
+                (() -> new Exception("No se puede actualizar la reserva"));
         p.setApellidos(reserva.getApellidos());
         return reservaRepositorio.save(reserva);
     }
 
     public Reserva anularReserva(Long codigo)throws Exception{
-        Reserva reserva = reservaRepositorio.findById(codigo).orElseThrow(() -> new Exception("No se puede eliminar"));
+        Reserva reserva = reservaRepositorio.findById(codigo).orElseThrow(() -> new Exception("No se puede reserva"));
         reservaRepositorio.delete(reserva);
         return reserva;
     }
+
 
 }
---
+++ Servicios/inkatrail/src/main/java/com/proyecto/inkatrail/rest/ReservaController.java
@@ -0,0 +1,72 @@
+package com.proyecto.inkatrail.rest;
+
+import com.proyecto.inkatrail.entidades.Reserva;
+import com.proyecto.inkatrail.negocio.Negocio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ReservaController {
+    @Autowired
+    private Negocio negocio;
+    Logger logger = LoggerFactory.getLogger(RestController.class);
+
+    @GetMapping("/reservas")
+    public List<Reserva> obtenerReservas(){
+        return negocio.obtenerReservas();
+    }
+
+    @PostMapping("/reserva")
+    public Reserva crearReserva(@RequestBody Reserva reserva){
+        Reserva p;
+        logger.debug("Reserva generada");
+        p = negocio.crearReserva(reserva);
+        return p;
+    }
+
+    @PutMapping("/producto")
+    public Reserva actualizarReserva(@RequestBody Reserva reserva){
+        Reserva p;
+        try {
+            logger.debug("Actualizando reserva");
+            p = negocio.actualizarReserva(reserva);
+            logger.debug("Reserva actualizada");
+        }catch (Exception e){
+            logger.error("Error de actualizacion en reserva");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Lo sentimos no se pudo actualizar la reserva");
+        }
+        return p;
+    }
+
+    @DeleteMapping("/reserva/{codigo}")
+    public Reserva anularReserva(@PathVariable(value = "codigo") Long codigo){
+        logger.debug("Anulando reserva");
+        try {
+            return negocio.anularReserva(codigo);
+        }catch (Exception e){
+            logger.error("Error al eliminar reserva");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Lo sentimos no se pudo eliminar la reserva");
+        }
+    }
+
+    @GetMapping("/producto/{codigo}")
+    public Reserva obtenerEntidad(@PathVariable(value = "codigo") Long codigo){
+        Reserva p;
+        try {
+            logger.debug("Buscando reserva");
+            p = negocio.obtenerReserva(codigo);
+            logger.debug("Reserva encontrado");
+        }catch (Exception e){
+            logger.error("Error buscando reserva");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Lo sentimos no se pudo encontrar la reserva");
+        }
+        return p;
+    }
+}
