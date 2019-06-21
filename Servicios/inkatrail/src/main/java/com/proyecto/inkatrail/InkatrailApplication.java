5ee459deeeb1736e43462433d154d06ca0169a94
Rolando Benigno <rbenigno@gmail.com>
2019-06-20 23:04:48 -0500

    Desarrollo de Servicios SOA

---
+++ Servicios/inkatrail/src/main/java/com/proyecto/inkatrail/InkatrailApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.inkatrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkatrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InkatrailApplication.class, args);
+	}
+
+}
