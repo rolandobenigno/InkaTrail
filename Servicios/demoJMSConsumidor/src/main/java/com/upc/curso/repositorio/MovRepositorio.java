6d5784053daee91868a275ea978db9147ca52824
Rolando Benigno <rbenigno@gmail.com>
2019-07-12 13:14:02 -0500

    Eliminando JMSConsumidor de colas

--- Servicios/demoJMSConsumidor/src/main/java/com/upc/curso/repositorio/MovRepositorio.java
+++
@@ -1,8 +0,0 @@
-package com.upc.curso.repositorio;
-
-import com.upc.curso.entidades.Movimiento;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MovRepositorio extends CrudRepository<Movimiento, Long> {
-
-}
